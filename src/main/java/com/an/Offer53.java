af21c86c97fe3a4a4446483397927800a9987f1f
an.ouyang <an.ouyang@zuopeng.com>
2021-01-28 18:05:38 +0800

    offer53

---
+++ src/main/java/com/an/Offer53.java
@@ -0,0 +1,32 @@
+package com.an;
+
+/**
+ * 统计一个数字在排序数组中出现的次数。
+ * <p>
+ *
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [5,7,7,8,8,10], target = 8
+ * 输出: 2
+ * 示例2:
+ * <p>
+ * 输入: nums = [5,7,7,8,8,10], target = 6
+ * 输出: 0
+ *
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= 数组长度 <= 50000
+ * <p>
+ *
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Offer53 {
+    public int search(int[] nums, int target) {
+        return 1;
+    }
+}
