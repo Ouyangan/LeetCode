45eb8859b41ad38bc89a1ef79b82b3412335b3cd
an.ouyang <an.ouyang@zuopeng.com>
2021-01-28 17:03:39 +0800

    offer03

--- src/main/java/com/an/Offer04.java
+++ src/main/java/com/an/Offer04.java
@@ -1,7 +1,6 @@
 package com.an;
 
 /**
- * https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/
  * <p>
  * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
  * <p>
@@ -25,11 +24,10 @@ package com.an;
  * 0 <= n <= 1000
  * <p>
  * 0 <= m <= 1000
+ * <p>
+ * https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/
  */
 public class Offer04 {
-    public static void main(String[] args) {
-
-    }
 
     public boolean findNumberIn2DArray(int[][] matrix, int target) {
         //校验参数
