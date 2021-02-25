030e4fe6954e4baf1123f4cba980f428ea80a1a7
an.ouyang <an.ouyang@zuopeng.com>
2021-02-25 17:39:18 +0800

    快速排序

--- src/main/java/com/an/Sort.java
+++ src/main/java/com/an/Sort.java
@@ -10,14 +10,61 @@ import java.util.Random;
 public class Sort {
     public static void main(String[] args) {
 
-        System.out.println("========bubbleSort========");
-        bubbleSort(createArray(10));
-        System.out.println("====insertSort=====");
-        insertSort(createArray(10));
-        System.out.println("=====selectSort=====");
-        selectSort(createArray(10));
-        System.out.println("======mergeSort=====");
-        mergeSort(createArray(10));
+//        System.out.println("========bubbleSort========");
+//        bubbleSort(createArray(10));
+//        System.out.println("====insertSort=====");
+//        insertSort(createArray(10));
+//        System.out.println("=====selectSort=====");
+//        selectSort(createArray(10));
+//        System.out.println("======mergeSort=====");
+//        mergeSort(createArray(10));
+        System.out.println("======quickSort=========");
+        quickSort(createArray(10));
+    }
+
+    /**
+     * 快速排序
+     * O(n^log^n)
+     */
+    public static Integer[] quickSort(Integer[] array) {
+        Integer[] integers = quickSort(array, 0, array.length - 1);
+        return integers;
+    }
+
+    public static Integer[] quickSort(Integer[] array, int left, int right) {
+        if (left < right) {
+            int p = partition(array, left, right);
+            quickSort(array, left, p - 1);
+            quickSort(array, p + 1, right);
+        }
+        return array;
+    }
+
+    private static int partition(Integer[] array, int left, int right) {
+        //找基准点
+        //排序基准点右边数组
+        int pivot = left;
+        int index = pivot + 1;
+
+        for (int i = index; i <= right; i++) {
+            //如果当前元素小于基准值,则将元素往左边已处理区域末尾
+            //否则指针不移动
+            if (array[i] < array[pivot]) {
+                //进行交换,
+                swap(array, i, index);
+                index++;
+            }
+        }
+        //index-1的位置就是最后
+        swap(array, pivot, index - 1);
+        return index - 1;
+    }
+
+
+    private static void swap(Integer[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
     /**
@@ -201,6 +248,10 @@ public class Sort {
         }
         System.out.println(builder.toString());
         return integers;
+    }
+
+    private static Integer[] createStaticArray() {
+        return new Integer[]{4, 3, 1, 9, 2, 6};
     }
 
 }
